ea84203b601ba1ded70ee4847db24f962acded7a
Ravindra Saxena <saxena.ravindra@gmail.com>
2019-07-26 14:31:47 +0530

    Add project files.

---
+++ HashDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HashDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello";
+            string str2 = "Hello";
+
+            Console.WriteLine($"String: {str1} -> Hashcode ({str1.GetHashCode()})");
+            Console.WriteLine($"String: {str2} -> Hashcode ({str2.GetHashCode()})");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OopsDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace OopsDemo
+{
+    public class A
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("A - Method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("A - Method2");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("A - Method3");
+        }
+
+    }
+
+    public class B : A
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("B - Method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("B - Method2");
+        }
+
+        public new void Method3()
+        {
+            Console.WriteLine("B - Method3");
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.Method1(); //"B - Method1"
+            a.Method2(); //"A - Method2"
+            a.Method3(); //"A - Method3"
+
+            Console.ReadLine();
+        }
+    }
+}
